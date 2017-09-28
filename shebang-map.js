48cf7ec9101343f3711f230e768faf75efd80651
Kiril Vatev <vatev.1@gmail.com>
2017-09-28 18:44:50 -0400

    reordering for organization

--- shebang-map.js
+++ shebang-map.js
@@ -2,24 +2,29 @@
 
 define(function (require, exports, module) {
   module.exports = {
-    '#!/usr/bin/env node': 'javascript',
-    '#!/usr/bin/node': 'javascript',
-    '#!/usr/bin/env groovy': 'groovy',
-    '#!/usr/bin/groovy': 'groovy',
-    '#!/usr/bin/env python': 'python',
-    '#!/usr/bin/python': 'python',
     '#!/usr/bin/env bash': 'bash',
     '#!/usr/bin/bash': 'bash',
     '#!/usr/bin/env sh': 'bash',
     '#!/usr/bin/sh': 'bash',
     '#!/bin/bash': 'bash',
     '#!/bin/sh': 'bash',
+
+    '#!/usr/bin/env groovy': 'groovy',
+    '#!/usr/bin/groovy': 'groovy',
+
     '#!/usr/bin/env stack': 'haskell',
     '#!/usr/bin/stack': 'haskell',
     '#!/usr/bin/env runhaskell': 'haskell',
     '#!/usr/bin/runhaskell': 'haskell',
+
+    '#!/usr/bin/env node': 'javascript',
+    '#!/usr/bin/node': 'javascript',
+
     '#!/usr/bin/env perl': 'perl',
     '#!/usr/bin/perl': 'perl',
     '#!/usr/local/bin/perl': 'perl',
+
+    '#!/usr/bin/env python': 'python',
+    '#!/usr/bin/python': 'python',
   };
 });
